ca7970b5b53d353302a6910abbc8d85d2bd3f68d
sidharthkk <96009053+sidharthkk@users.noreply.github.com>
2023-06-18 16:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,51 @@ from flask import Flask, render_template
 import subprocess
 import os
 import signal
+import sys
+
 
 app = Flask(__name__)
+
+letter_process = None
+words_process = None
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/run_letter')
 def run_letter():
-    subprocess.run(['python', 'letters.py'])
-    return 'Letter.py is running'
+    global letter_process
+    if letter_process is None or letter_process.poll() is not None:
+        letter_process = subprocess.Popen([sys.executable, 'letters.py'])
+        return 'Letter.py is running'
+    else:
+        return 'Letter.py is already running'
+
 
 @app.route('/run_words')
 def run_words():
-    subprocess.run(['python', 'words.py'])
-    return 'Words.py is running'
+    global words_process
+    if words_process is None or words_process.poll() is not None:
+        words_process = subprocess.Popen([sys.executable, 'words.py'])
+        return 'Words.py is running'
+    else:
+        return 'Words.py is already running'
+
 
 @app.route('/clear_program')
 def clear_program():
-    os.kill(os.getpid(), signal.SIGINT)
+    global letter_process, words_process
+    if letter_process is not None:
+        letter_process.kill()
+        letter_process = None
+    if words_process is not None:
+        words_process.kill()
+        words_process = None
     return 'Program terminated'
+
 
 if __name__ == '__main__':
     app.run()
